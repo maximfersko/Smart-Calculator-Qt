ada099261d79ee837be914999d1ee247bcced818
maximfersko <79246330+maximfersko@users.noreply.github.com>
2023-04-22 20:52:43 +0800

    refactoring

--- src/smartCalcMvc/controller/controller.h
+++ src/smartCalcMvc/controller/controller.h
@@ -11,13 +11,13 @@ public:
   using data_t_credit = creditModel::data_c_t_;
 
 private:
-  SmartCalc *model_;
-  creditModel *creditModel_;
-  deposit *depositModel_;
+  std::unique_ptr<SmartCalc> model_;
+  std::unique_ptr<creditModel> creditModel_;
+  std::unique_ptr<deposit> depositModel_;
 
 public:
-  controller()
-      : model_(nullptr), creditModel_(nullptr), depositModel_(nullptr) {}
+  controller() = default;
+  ~controller() = default;
   controller(SmartCalc *model, creditModel *modelCredit, deposit *depositModel);
   double RPN(std::string str);
   double RPN(std::string str, double X);
@@ -30,7 +30,6 @@ public:
   graphBuilder(std::vector<double> &Data, std::string str);
   data_t_credit Annuity(double summa, double period, double rate);
   data_t_credit deffirentated(double summa, double rate, double period);
-  ~controller() {}
 };
 
 }; // namespace model
